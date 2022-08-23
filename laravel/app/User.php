5bf121de153c051352442e61e4fd9f0c8eb35913
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2022-08-23 15:19:05 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modificado:    ../.gitignore
    	modificado:    app/User.php
    	novo ficheiro: config/cors.php
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -5,35 +5,83 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $email_verified_at
+ * @property string $password
+ * @property string $remember_token
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class User extends Authenticatable
 {
     use Notifiable;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast.
      *
-     * @var array
+     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Set the password to use when connecting (if needed).
+     *
+     * @param string $password
+     *
+     * @return $this
+     */
+    public function setPasswordAttribute($password)
+    {
+        // $this->attributes['password'] = bcrypt($password);
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
