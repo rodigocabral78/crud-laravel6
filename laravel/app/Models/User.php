bb10df7d040df5999b506999cc2e952e5bc8a90a
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2022-08-23 13:57:05 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modificado:    .env.example
    	novo ficheiro: .env.sqlite
    	modificado:    app/Http/Controllers/Auth/RegisterController.php
    	novo ficheiro: app/Models/User.php
    	modificado:    composer.json
    	modificado:    composer.lock
    	modificado:    config/app.php
    	modificado:    config/auth.php
    	novo ficheiro: config/eloquent_model_generator.php
    	modificado:    database/factories/UserFactory.php
    	novo ficheiro: resources/lang/pt-BR.json
    	novo ficheiro: resources/lang/pt-BR/auth.php
    	novo ficheiro: resources/lang/pt-BR/pagination.php
    	novo ficheiro: resources/lang/pt-BR/passwords.php
    	novo ficheiro: resources/lang/pt-BR/validation.php
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

---
+++ laravel/app/Models/User.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $email_verified_at
+ * @property string $password
+ * @property string $remember_token
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Set the password to use when connecting (if needed).
+     *
+     * @param string $password
+     *
+     * @return $this
+     */
+    public function setPasswordAttribute($password)
+    {
+        // $this->attributes['password'] = bcrypt($password);
+        $this->attributes['password'] = Hash::make($password);
+    }
+}
