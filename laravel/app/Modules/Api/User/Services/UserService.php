4f299f05caf9ea952ae673884a98937879bb3583
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2022-08-24 22:41:03 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	novo ficheiro: app/Http/Controllers/HomeController.php
    	modificado:    app/Modules/Api/User/Services/UserService.php
    	modificado:    composer.json
    	modificado:    package.json
    	novo ficheiro: public/css/app.css
    	novo ficheiro: public/js/app.js
    	novo ficheiro: public/mix-manifest.json
    	modificado:    resources/js/bootstrap.js
    	novo ficheiro: resources/sass/_variables.scss
    	modificado:    resources/sass/app.scss
    	novo ficheiro: resources/views/auth/login.blade.php
    	novo ficheiro: resources/views/auth/passwords/confirm.blade.php
    	novo ficheiro: resources/views/auth/passwords/email.blade.php
    	novo ficheiro: resources/views/auth/passwords/reset.blade.php
    	novo ficheiro: resources/views/auth/register.blade.php
    	novo ficheiro: resources/views/auth/verify.blade.php
    	novo ficheiro: resources/views/home.blade.php
    	novo ficheiro: resources/views/layouts/app.blade.php
    	modificado:    routes/web.php
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- laravel/app/Modules/Api/User/Services/UserService.php
+++ laravel/app/Modules/Api/User/Services/UserService.php
@@ -9,84 +9,98 @@ use Illuminate\Http\Request;
 
 class UserService
 {
-	public function index(Request $request)
-	{
-		$userSearchService = new UserSearchService();
-		#return $userSearchService->search(User::with([]), $request);
-		$data = $userSearchService(User::with([]), $request)->toArray();
-		return [
-			'total_count' => $data['total'],
-			'limit' => $data['per_page'],
-			'data' => $data['data'],
-			'paginate' => [
-				'path_url' => $data['path'],
-				'first_page' => $data['first_page_url'],
-				'prev_page' => $data['prev_page_url'],
-				'current_page' => $data['current_page'],
-				'next_page' => $data['next_page_url'],
-				'last_page' => $data['last_page_url'],
-				'from' => $data['from'],
-				'to' => $data['to'],
-			],
-		];
-	}
+    public function index(Request $request)
+    {
+        $userSearchService = new UserSearchService();
+        #return $userSearchService->search(User::with([]), $request);
+        $data = $userSearchService(User::with([]), $request)->toArray();
+        return [
+            'status' => 'success',
+            'code' => 200,
+            'message' => '',
+            'total_count' => $data['total'],
+            'limit' => $data['per_page'],
+            'offset' => $data['current_page'],
+            'current_page' => $data['current_page'],
+            'fields' => [
+                'action' => 'Action',
+                'id' => 'ID',
+                'name' => 'Name',
+                'created_at' => 'Created At',
+                'updated_at' => 'Updated At',
+            ],
+            'items' => $data['data'],
+            'data' => $data['data'],
+            'paginate' => [
+                'path_url' => $data['path'],
+                'first_page' => $data['first_page_url'],
+                'prev' => $data['prev_page_url'],
+                'prev_page' => $data['prev_page_url'],
+                'next' => $data['next_page_url'],
+                'next_page' => $data['next_page_url'],
+                'last_page' => $data['last_page_url'],
+                'from' => $data['from'],
+                'to' => $data['to'],
+            ],
+        ];
+    }
 
-	public function show($id)
-	{
-		# $user = User::findOrFail($id);
-		$user = User::find($id);
+    public function show($id)
+    {
+        # $user = User::findOrFail($id);
+        $user = User::find($id);
 
-		return $user;
-	}
+        return $user;
+    }
 
-	public function store($request)
-	{
-		$validator = Validator::make($request->all(), [
-		]);
+    public function store($request)
+    {
+        $validator = Validator::make($request->all(), [
+        ]);
 
-		if($validator->errors()->count()) {
-			throw new \Exception($validator->errors()->first(),400);
-		}
+        if ($validator->errors()->count()) {
+            throw new \Exception($validator->errors()->first(), 400);
+        }
 
-		if(!$validator->errors()->count()) {
-			$data = [
-				"name" => $request->name,
-				"email" => $request->email,
-				"email_verified_at" => $request->email_verified_at,
-				"password" => $request->password,
-				"remember_token" => $request->remember_token,
-			];
-			return User::create($data);
-		}
-	}
+        if (!$validator->errors()->count()) {
+            $data = [
+                "name" => $request->name,
+                "email" => $request->email,
+                "email_verified_at" => $request->email_verified_at,
+                "password" => $request->password,
+                "remember_token" => $request->remember_token,
+            ];
+            return User::create($data);
+        }
+    }
 
-	public function update($request, $id)
-	{
-		# $user = User::find($id);
+    public function update($request, $id)
+    {
+        # $user = User::find($id);
 
-		$validator = Validator::make($request->all(), [
-		]);
+        $validator = Validator::make($request->all(), [
+        ]);
 
-		if($validator->errors()->count()) {
-			throw new \Exception($validator->errors()->first(),400);
-		}
+        if ($validator->errors()->count()) {
+            throw new \Exception($validator->errors()->first(), 400);
+        }
 
-		if(!$validator->errors()->count()) {
-			$data = [
-				"name" => $request->name,
-				"email" => $request->email,
-				"email_verified_at" => $request->email_verified_at,
-				"password" => $request->password,
-				"remember_token" => $request->remember_token,
-			];
-			$user = User::find($id)->update($data);
-		}
-		return $user;
-	}
+        if (!$validator->errors()->count()) {
+            $data = [
+                "name" => $request->name,
+                "email" => $request->email,
+                "email_verified_at" => $request->email_verified_at,
+                "password" => $request->password,
+                "remember_token" => $request->remember_token,
+            ];
+            $user = User::find($id)->update($data);
+        }
+        return $user;
+    }
 
-	public function destroy($id)
-	{
-		$user = User::find($id)->delete();
-		return $user;
-	}
+    public function destroy($id)
+    {
+        $user = User::find($id)->delete();
+        return $user;
+    }
 }
